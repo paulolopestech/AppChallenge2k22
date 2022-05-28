b80bfafaeb4110915124c20b11a0efee0ab51ce5
Manu <65041975+ManuelaGripp@users.noreply.github.com>
2022-05-28 16:26:11 -0300

    :construction: planos

--- sat-finder/src/App.js
+++ sat-finder/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
+import Titulo from './componentes/titulo';
+import Mapa from './componentes/mapa';
+import ColorToggleButton from './componentes/tipos';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <Titulo texto = "REGIÃO"/>
+      <Mapa/>
+      <Titulo texto = "PLANOS" />
+      <ColorToggleButton/>
+      </div>
   );
 }
 
