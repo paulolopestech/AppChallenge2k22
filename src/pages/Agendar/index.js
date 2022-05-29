65bd758443f988d4a943d06e5f224e9fb55da045
Manu <65041975+ManuelaGripp@users.noreply.github.com>
2022-05-29 17:12:22 -0300

    :sparkles: feat  pagina Instalador

--- src/pages/Agendar/index.js
+++ src/pages/Agendar/index.js
@@ -2,6 +2,10 @@ import '../../App.css';
 import React from 'react';
 import { useLocation } from 'react-router';
 
+import Titulo from '../../componentes/titulo'
+
+import './agendar.css'
+
 function Agendar(props) {
     const location = useLocation();
     const data = location.state;
@@ -9,22 +13,22 @@ function Agendar(props) {
     const plano = data.plano;
 
   return(
-      <div>
-          INSTALADOR
-          <div>
-              <p>NOTA: {instalador.rating}</p>
-              <p>NOME: {instalador.name}</p>
-              <p>PREÇO/KM: {instalador.price_per_km}</p>
+      <div >
+          <Titulo texto = 'Instalador' />
+          <div className='InfoInstalado' >
+              <h2>{instalador.name}</h2>
+              <p>Nota: {instalador.rating}</p>
+              <p>Preço/KM: R${instalador.price_per_km}</p>
           </div>
 
-          PLANO
-          <div>
-              <p>PROVEDOR: {plano.isp}</p>
-              <p>PREÇO: {plano.price_per_month}</p>
-              <p>VELOCIDADE DE DOWNLOAD{plano.download_speed}</p>
+          <div className='InformacaoPLano' >
+          <h2>PLANO</h2>
+              <p>Provedor: {plano.isp}</p>
+              <p>Preço:R$  {plano.price_per_month}</p>
+              <p>Velocida de Download: {plano.download_speed}</p>
           </div>
 
-          <button>AGENDAR</button>
+          <button className='botaoAgendar' >CONRTATAR</button>
       </div>
   );
 }
