d90d1b006a41dc42d8e635343bd0f679e471a1e2
Paulo Lopes <phlj.junior@gmail.com>
2022-05-29 14:23:01 -0300

    :recycle: remove coordinates console.log

--- src/utils/coordinatesUtils.js
+++ src/utils/coordinatesUtils.js
@@ -1,6 +1,5 @@
 export default function coordinatesUtils () {
     let coordinates;
     navigator.geolocation.getCurrentPosition((position) => coordinates = position.coords);
-    console.log(coordinates.latitude)
     return coordinates;
 }
